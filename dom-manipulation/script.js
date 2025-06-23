d1ede1354f037121f9289fbbbaae3c88c23e41f8
Ruby Tater <rubytater10@gmail.com>
2025-06-23 19:06:04 +0000

    upload

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -59,3 +59,14 @@ function createElement() {
     document.getElementById("addQuoteBtn").addEventListener("click", addQuote);
   });
   
+  // upload a JSON file
+  function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
